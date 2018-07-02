4e0993365052fa57d970d0185d6afbe2acba7b35
David Tumpowsky <tumpowskydavid@gmail.com>
2018-07-02 15:58:29 -0700

    inverse data flow to dynamically create new help ticket and add to list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,33 +4,49 @@ import TicketList from './TicketList';
 import NewTicketControl from './NewTicketControl';
 import { Switch, Route } from 'react-router-dom';
 import Error404 from './Error404';
+import PropTypes from 'prop-types';
 
-function App() {
-  return (
-    <div>
-      <style global jsx >{`
-        body {
-          font-family: Helvetica;
-        }
-        a {
-          color: #888;
-          text-decoration: none;
-        }
-      `}</style>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={TicketList} />
-        <Route path='/newticket' component={NewTicketControl} />
-        <Route component={Error404} />
-      </Switch>
-      <div style={{margin: '19px auto 0', width: 142}}>
-        <a href="https://medium.com/" target="_blank">
-          <div className="box">
-          </div>
-        </a>
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterTicketList:[]
+    };this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+  }
+
+  handleAddingNewTicketToList(newTicket){
+    var newMasterTicketList = this.state.masterTicketList.slice();
+    newMasterTicketList.push(newTicket);
+    this.setState({masterTicketList: newMasterTicketList});
+  }
+
+  render(){
+    return (
+      <div>
+        <style global jsx >{`
+          body {
+            font-family: Helvetica;
+          }
+          a {
+            color: #888;
+            text-decoration: none;
+          }
+        `}</style>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
+          <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route component={Error404} />
+        </Switch>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+NewTicketControl.propTypes = {
+  onNewTicketCreation: PropTypes.func
+};
 
 export default App;
